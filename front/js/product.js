4571fd261c1a03bb7594a957873376d526d17e8c
Webandsea13 <taysse.13@gmail.com>
2022-01-20 16:07:57 +0100

    config local storage (sans cumul)

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,4 @@
+//******************affichage de la page produit************************
 // Récupérer l'url
 const Url = window.location.href;
 console.log(Url);
@@ -12,8 +13,7 @@ console.log(idProduct);
 
 // Fetch le produit d'id {id}
 const UrlFetch = "http://localhost:3000/api/products/" + idProduct;
-const place = document.querySelector(".item__img");
-console.log(place);
+console.log(UrlFetch);
 
 fetch(UrlFetch)
 	.then(function (res) {
@@ -23,12 +23,16 @@ fetch(UrlFetch)
 		console.log(product);
 		console.log(product.imageUrl);
 		console.log(product.name);
-		// Insérer les éléments correspondants dans le HTML
-		place.insertAdjacentHTML(
-			"beforeend",
-			`<img src= "${product.imageUrl}" alt="${product.altTxt}">`
-		);
+		// Insérer les éléments correspondants dans le HTML :
+		//insertion image
+		document
+			.querySelector(".item__img")
+			.insertAdjacentHTML(
+				"beforeend",
+				`<img src= "${product.imageUrl}" alt="${product.altTxt}">`
+			);
 
+		//insertion détails éléments
 		document
 			.getElementById("title")
 			.insertAdjacentHTML("beforeend", product.name);
@@ -41,6 +45,7 @@ fetch(UrlFetch)
 			.getElementById("description")
 			.insertAdjacentHTML("beforeend", product.description);
 
+		//insertion des couleurs dans le menu déroulant
 		for (let color of product.colors) {
 			document
 				.getElementById("colors")
@@ -53,4 +58,43 @@ fetch(UrlFetch)
 
 	.catch(function (err) {
 		alert("Erreur:" + err);
+	});
+
+//***************gestion de l'ajout au local storage par le bouton envoyer********************
+
+//creation d'un tableau vide qui contiendra tous les choix à ajouter au panier
+let panier = [];
+/*
+//fonction ajouter un choix au panier ou cumuler quantité si choix déjà existant dans le panier(id et couleur identiques)
+function ajout_panier(nouveauchoix) {
+	for (let object of panier) {
+		if (object[0] === idProduct && object[1] === choixColor) {
+			object[2] += choixQuantity;
+		} else {
+			panier.push(nouveauchoix);
+		}
+	}
+}
+*/
+
+//écoute du bouton ajouter au panier et recupération d'un choix utilisateur
+document
+	.getElementById("addToCart")
+	.addEventListener("click", function (event) {
+		event.preventDefault();
+		//récupération du choix utilisateur
+		let choixColor = document.getElementById("colors").value;
+		console.log(choixColor);
+		let choixQuantity = document.getElementById("quantity").value;
+		console.log(choixQuantity);
+		//création d'un tableau pour stocker le choix utilisateur
+		let choix = [idProduct, choixColor, choixQuantity];
+		console.log(choix);
+		//ajout du choix dans le tableau qui cumule les différents choix
+		panier.push(choix); /*ou appel de la fonction ajout_panier(choix);*/
+		console.log(panier);
+
+		//envoi du tableau dans local storage
+		let panierLinea = JSON.stringify(panier);
+		localStorage.setItem("panierLocalstorage", panierLinea);
 	});
