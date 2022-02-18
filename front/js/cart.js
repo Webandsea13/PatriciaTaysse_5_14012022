5b116ab251741c576fc1031c035863132a046a9b
Webandsea13 <taysse.13@gmail.com>
2022-02-18 15:14:31 +0100

    projet opérationnel

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,39 +1,86 @@
 // ! requires cart-library.js
-// !requires form_validation.js
+// ! requires form_validation.js
+
+//creation tableau des articles pour regrouper les données venant du localStorage et du fetch
+let articlesArray = [];
+//declaration tableau panier avec élément provenant du localstorage (id, couleur, quantité)
+let cart;
 
 ////////////////////////////////////////////////////////////////////////////////////
 /////definition des fonctions utilisées dans le fichier////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////
 
-//définition fonction qui calcule et affiche le prix total et la quantité totale des produits du panier
+//fonction qui réunit dans un objet article les informations du fetch
+//et du local storage, article pushé dans un tableau articlesArray
 /**
- * Met à jour le prix total et la quantité totale
+ * récupération des données du localStorage et du backend par un fetch
+ * remplissage tableau articlesAray avec objet article
  */
-const refreshPriceAndQuantity = function () {
-	let totalQuantity = 0;
-	let totalPrice = 0;
-	for (let p of cart) {
-		//cumul nombre articles
-		totalQuantity += parseInt(p.quantity);
+const getMixedData = async function () {
+	//récupération panier localStorage
+	cart = loadCart();
+	console.log(cart);
 
-		//cumul prix
-		//on récupère le prix stocké dans priceProducts grace à la clé p.id
-		//pour le mettre dans la constante prixProduit
-		const priceProduct = priceProducts[p.id];
-		let price = priceProduct * p.quantity;
-		totalPrice += price;
-	}
+	//récupération des promesses des fetchs pour chaque élément du panier
+	//et gestion de l'objet article et du tableau articlesArray
+	await Promise.all(
+		cart.map(async function (cartElement) {
+			const idProduct = cartElement.id;
+			const UrlFetch = "http://localhost:3000/api/products/" + idProduct;
+			const oneFetch = await fetch(UrlFetch)
+				.then(function (res) {
+					return res.json();
+				})
+				.catch(function (err) {
+					alert(err);
+				});
+			let article = {
+				id: cartElement.id,
+				color: cartElement.color,
+				quantity: cartElement.quantity,
+				price: oneFetch.price,
+				name: oneFetch.name,
+				url: oneFetch.imageUrl,
+				altTxt: oneFetch.altTxt,
+			};
+			articlesArray.push(article);
 
-	//affichage nombre articles
-	document.getElementById("totalQuantity").textContent = `${totalQuantity}`;
-	//affichage du prix total
-	document.getElementById("totalPrice").textContent = `${totalPrice}`;
+			console.log(articlesArray);
+		})
+	);
+	/*for (let c of cart) {
+		const idProduct = c.id;
+		const UrlFetch = "http://localhost:3000/api/products/" + idProduct;
+
+		const oneFetch = await fetch(UrlFetch)
+			.then(function (res) {
+				return res.json();
+			})
+			.catch(function (err) {
+				alert(err);
+			});
+
+		let article = {
+			id: c.id,
+			color: c.color,
+			quantity: c.quantity,
+			price: oneFetch.price,
+			name: oneFetch.name,
+			url: oneFetch.imageUrl,
+			altTxt: oneFetch.altTxt,
+		};
+		articlesArray.push(article);
+
+		console.log(articlesArray);
+	}*/
 };
 
-//définition de la fonction exécutée au change de la quantité par l'utilisateur
+//fonction callback de l'événement modification de quantité
+//mise à jour de articlesArray et cart avec la nouvelle quantité
+//mise à jour des totaux du panier
 /**
  * Handler de changement de quantité d'un produit
- * @param {event} evenement changement quantité
+ * @param {*} event
  */
 const changeQuantityCallback = function (event) {
 	//trouver élément proche du clic et contenant les informations du produit concerné
@@ -42,21 +89,30 @@ const changeQuantityCallback = function (event) {
 	//récupérer id et couleur produit concerné
 	const chosenId = item.dataset.id;
 	const chosenColor = item.dataset.color;
-	//chercher l'élément dans le panier
-	const chosenItem = cart.find(
+	//chercher l'élément dans le tableau des articles
+	const chosenItem = articlesArray.find(
 		(x) => x.id == chosenId && x.color == chosenColor
 	);
-	//Appliquer la nouvelle quantité dans l'élément panier
-	chosenItem.quantity = event.target.value;
+	//Appliquer la nouvelle quantité dans le tableau d'articles
+	chosenItem.quantity = parseInt(event.target.value);
+	//chercher l'élément dans le panier
+	const chosenItem2 = cart.find(
+		(x) => x.id == chosenId && x.color == chosenColor
+	);
+	//Appliquer la nouvelle quantité dans l'élément panier localStorage
+	chosenItem2.quantity = parseInt(event.target.value);
 
 	//appel fonction sauvegarder panier
 	saveCart(cart);
 	console.log(cart);
+	console.log(articlesArray);
 	//recalculer prix total et quantité totale
 	refreshPriceAndQuantity();
 };
 
-//definition de la fonction exécutée à la suppression d'un produit du panier par l'utilisateur
+// fonction callback exécutée à la suppression d'un produit du panier par l'utilisateur
+//modification de articlesArray et cart
+//affichage du nouveau panier
 /**
  * Handler de suppression d'un produit du panier
  * @param {event} evenement suppression
@@ -72,22 +128,122 @@ const deleteItemCallback = function (event) {
 	);
 	console.log(result);
 	saveCart(result);
-	//rafraichir page ou afficher nouveau panier
-	window.location.reload();
+	//filter le tableau des articles sans l'element à supprimer
+	articlesArray = articlesArray.filter(
+		(n) => !(n.id === chosenId && n.color === chosenColor)
+	);
+	console.log(articlesArray);
+	//afficher nouveau panier
+	displayCart();
 };
 
-// définition de la fonction qui écoute le bouton Commander
 /**
- * écoute du click "commander"
+ * affichage des articles de articlesArray et des totaux
+ * écoute des événements de la page : modification, suppression, commande
  */
-const submitOrder = function () {
+const displayCart = function () {
+	document.getElementById("cart__items").innerHTML = "";
+	if (articlesArray.length == 0) {
+		let titre = document.querySelector("h1");
+		let precision = document.createElement("p");
+		let precisionText = document.createTextNode(
+			"Le panier est vide ! Retournez en page d'accueil choisir vos articles."
+		);
+		precision.appendChild(precisionText);
+		precision.style.color = "#FF6F61";
+		precision.style.backgroundColor = "white";
+		document
+			.getElementById("cartAndFormContainer")
+			.insertBefore(precision, document.querySelector(".cart"));
+	}
+
+	for (let article of articlesArray) {
+		//declaration de la constante contenant le html pour un produit du panier
+		const objectAsHTML = `<article class="cart__item" data-id=${article.id} data-color=${article.color}>
+				<div class="cart__item__img">
+				<img src="${article.url}" alt="${article.altTxt}">
+				</div>
+				<div class="cart__item__content">
+				<div class="cart__item__content__description">
+					<h2>${article.name}</h2>
+					<p>${article.color}</p>
+					<p>${article.price} €</p>
+				</div>
+				<div class="cart__item__content__settings">
+					<div class="cart__item__content__settings__quantity">
+					<p>Qté : </p>
+					<input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${article.quantity}>
+					</div>
+					<div class="cart__item__content__settings__delete">
+					<p class="deleteItem">Supprimer</p>
+					</div>
+				</div>
+				</div>
+				</article>`;
+
+		//insertion du html
+		document
+			.getElementById("cart__items")
+			.insertAdjacentHTML("beforeend", objectAsHTML);
+	}
+
+	refreshPriceAndQuantity();
+
+	/////////gestion des evenements de la page ///////////////
+
+	//**********************changement quantité par l'utilisateur*********************
+
+	//écoute click + appel fonction changeQuantityCallback
+	//récupérer dans un tableau les éléments susceptibles d'être cliqués +/-
+	let tabItemQuantity = document.querySelectorAll(".itemQuantity");
+
+	for (let itemQuantity of tabItemQuantity) {
+		itemQuantity.addEventListener("change", changeQuantityCallback);
+	}
+
+	//*************************suppression d'un article************* */
+	//récupérer dans un tableau les éléments susceptibles d'etre cliqués pour la suppression
+
+	let tabDeleteItems = document.querySelectorAll(".deleteItem");
+
+	tabDeleteItems.forEach(function (delIt) {
+		delIt.addEventListener("click", deleteItemCallback);
+	});
+};
+
+/**
+ * calculs et affichage du nombre total d'articles et du prix total du panier
+ */
+const refreshPriceAndQuantity = function () {
+	let totalQuantity = 0;
+	let totalPrice = 0;
+	for (let article of articlesArray) {
+		//cumul nombre articles
+		totalQuantity += parseInt(article.quantity);
+
+		//cumul prix
+		//on récupère le prix stocké dans priceProducts grace à la clé p.id
+		//pour le mettre dans la constante prixProduit
+
+		let price = article.price * article.quantity;
+		totalPrice += price;
+	}
+
+	//affichage nombre articles
+	document.getElementById("totalQuantity").textContent = `${totalQuantity}`;
+	//affichage du prix total
+	document.getElementById("totalPrice").textContent = `${totalPrice}`;
+};
+
+////////////gestion du formulaire/////////////////////
+function submitOrder() {
 	//selection du bouton envoyer
 	const btnOrder = document.querySelector("#order");
 
 	// Écoute  click pour pouvoir créer les éléments à envoyer au backend,
 	// contrôler, valider et envoyer le formulaire via formSubmitCallback
 	btnOrder.addEventListener("click", formSubmitCallback);
-};
+}
 
 //definition de la fonction exécutée au click du bouton commander
 /**
@@ -120,21 +276,45 @@ const formSubmitCallback = function (event) {
 	//verification du formulaire avec les fonctions définies dans form_validation.js
 	//si tous les champs du formulaires sont valides
 
-	if (
-		FirstNameControl(contact) &&
-		LastNameControl(contact) &&
-		addressControl(contact) &&
-		cityControl(contact) &&
-		mailControl(contact)
-	) {
-		//appel fonction pour envoyer les données contact et product au serveur
-		sendOrder(contact, products);
+	const validations = [
+		nameControl(
+			document.querySelector("#firstName"),
+			document.querySelector("#firstNameErrorMsg"),
+			"Prénom valide",
+			"Prénom invalide"
+		),
+
+		nameControl(
+			document.querySelector("#lastName"),
+			document.querySelector("#lastNameErrorMsg"),
+			"Nom valide",
+			"Nom invalide"
+		),
+		locationControl(
+			document.querySelector("#address"),
+			document.querySelector("#addressErrorMsg"),
+			"Adresse valide",
+			"Adresse invalide. Exemple : 10 rue du port"
+		),
+		locationControl(
+			document.querySelector("#city"),
+			document.querySelector("#cityErrorMsg"),
+			"Ville valide",
+			"Ville invalide. Exemple : 33510 Andernos-les-Bains"
+		),
+		mailControl(
+			document.querySelector("#email"),
+			document.querySelector("#emailErrorMsg"),
+			"Email valide",
+			"Email invalide. Exemple : contact@monsite.fr"
+		),
+	];
+
+	if (validations.includes(false)) {
+		return;
 	}
 
-	/**
-	 * requete POST, recupération numéro de commande et redirection sur la page confirmation
-	 */
-	function sendOrder(contact, products) {
+	function sendOrder() {
 		fetch("http://localhost:3000/api/products/order", {
 			method: "POST",
 			headers: {
@@ -159,115 +339,18 @@ const formSubmitCallback = function (event) {
 				alert(err);
 			});
 	}
+	sendOrder();
 };
 
-///////////////////////////////////////////////////////////////////////////////////////
-//*************afficher les objets du panier sur la page panier************************
-/////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////
+////lancement de la page////////////////////////
+////////////////////////////////////////////////
 
-//récupérer le panier dans le local storage via la fonction recupererPanier définie dans gestion_panier.js
-const cart = loadCart();
-console.log(cart);
-//si le panier est vide : affichage d'un message
-if (cart.length == 0) {
-	let titre = document.querySelector("h1");
-	let precision = document.createElement("p");
-	let precisionText = document.createTextNode(
-		"Le panier est vide ! Retournez en page d'accueil choisir vos articles."
-	);
-	precision.appendChild(precisionText);
-	precision.style.color = "red";
-	precision.style.backgroundColor = "white";
-	document
-		.getElementById("cartAndFormContainer")
-		.insertBefore(precision, document.querySelector(".cart"));
-}
+const onLoad = async function () {
+	await getMixedData();
+	displayCart();
+	submitOrder();
+};
 
-// cache des prix des produits pour les utiliser en dehors du fetch
-const priceProducts = {};
-
-//boucle pour récupérer les objects du panier
-//création du tableau contenant les promesses des fetches
-const fetches = [];
-
-for (let p of cart) {
-	//récupération de l'id du produit
-	const idProduct = p.id;
-
-	// Fetch le produit d'id {id}
-	const UrlFetch = "http://localhost:3000/api/products/" + idProduct;
-
-	const leFetch = fetch(UrlFetch)
-		.then(function (res) {
-			return res.json();
-		})
-		.then(function (product) {
-			//declaration de la constante contenant le html pour un produit du panier
-			//p est un objet du panier provenant du localstorage, product est un objet provenant du backend
-			console.log(product);
-
-			const objectAsHTML = `<article class="cart__item" data-id=${p.id} data-color=${p.color}>
-            <div class="cart__item__img">
-            <img src="${product.imageUrl}" alt="${product.altTxt}">
-            </div>
-            <div class="cart__item__content">
-            <div class="cart__item__content__description">
-                <h2>${product.name}</h2>
-                <p>${p.color}</p>
-                <p>${product.price} €</p>
-            </div>
-            <div class="cart__item__content__settings">
-                <div class="cart__item__content__settings__quantity">
-                <p>Qté : </p>
-                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${p.quantity}>
-                </div>
-                <div class="cart__item__content__settings__delete">
-                <p class="deleteItem">Supprimer</p>
-                </div>
-            </div>
-            </div>
-            </article>`;
-
-			//insertion du html
-			document
-				.getElementById("cart__items")
-				.insertAdjacentHTML("beforeend", objectAsHTML);
-
-			// stocker le prix en "cache" dans l'objet PriceProducts, clé=id et valeur=prix
-			priceProducts[p.id] = product.price;
-			console.log(priceProducts);
-
-			//**********************changement quantité par l'utilisateur*********************
-
-			//écoute click + appel fonction changeQuantityCallback
-			//récupérer dans un tableau les éléments susceptibles d'être cliqués +/-
-			let tabItemQuantity = document.querySelectorAll(".itemQuantity");
-
-			for (let itemQuantity of tabItemQuantity) {
-				itemQuantity.addEventListener("change", changeQuantityCallback);
-			}
-
-			//*************************suppression d'un article************* */
-			//récupérer dans un tableau les éléments susceptibles d'etre cliqués pour la suppression
-
-			let tabDeleteItems = document.querySelectorAll(".deleteItem");
-
-			tabDeleteItems.forEach(function (delIt) {
-				delIt.addEventListener("click", deleteItemCallback);
-			});
-		})
-		.catch(function (err) {
-			alert("Erreur:" + err);
-		});
-
-	fetches.push(leFetch);
-}
-//ensemble des promesses résolues
-Promise.all(fetches).then(() => {
-	//calcul et affichage quantité totale et prix total
-	refreshPriceAndQuantity();
-});
-
-//**************appel fonction action du bouton commander*********************** */
-
-submitOrder();
+//appel fonction lancement de la page
+onLoad();
